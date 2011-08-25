c35a73df8fb7c86c84b8764d329270e4df3a8c41
Manabu Takayama <learn.libra@gmail.com>
2011-08-25 16:15:57 +0900

    Changed the format of a error message.
    A part of #19.

--- src/condition.lisp
+++ src/condition.lisp
@@ -10,14 +10,14 @@
 (defun print-error-message (out stream pos expect)
   (format out "The parser encountered the unexpected ")
   (if pos
-      (format out "input at ~a." (numerical-position pos stream))
-      (format out "end of input."))
+      (format out "input at ~a.~%" (numerical-position pos stream))
+      (format out "end of input.~%"))
   (let ((*print-circle* nil)
         (unexpected (if pos (car (parser-stream-car pos)) "the end of input")))
     (if expect
-        (format out " It expects ~{~a~}, but got ~a."
+        (format out "It expects ~{~a~}, but got ~a."
                 (intersperse ", " expect " or ") unexpected)
-        (format out " It doesn't expect ~a." unexpected))))
+        (format out "It doesn't expect ~a." unexpected))))
 
 (define-condition parser-error (error)
   ((stream   :initarg :stream
