c7008a96e6f47664f370f487745607fa395f70f6
Manabu Takayama <learn.libra@gmail.com>
2012-04-15 01:49:21 +0900

    Refactored SEP-BY1 and MANY1 with MLET*.
    This is a change for #20.

--- src/combinator.lisp
+++ src/combinator.lisp
@@ -5,14 +5,14 @@
           (unit nil)))
 
 (defun sep-by1 (parser sep)
-  (seq/bind (x  <- parser)
-            (xs <- (many (seqn sep parser)))
-            (unit (cons x xs))))
+  (mlet* ((x parser)
+          (xs (many (seqn sep parser))))
+    (unit (cons x xs))))
 
 (defun many1 (parser)
-  (seq/bind (r  <- parser)
-            (rs <- (many parser))
-            (unit (cons r rs))))
+  (mlet* ((r parser)
+          (rs (many parser)))
+    (unit (cons r rs))))
 
 (defun skip-many1 (parser)
   (seqn parser (skip-many parser)))
